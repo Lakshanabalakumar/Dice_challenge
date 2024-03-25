29754fbec3b05f445c1339ffa979e6a9c0319f0a
Lakshanabalakumar <136481451+Lakshanabalakumar@users.noreply.github.com>
2024-03-25 22:27:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ function generateRandomNumber(){
+     var randomNumber=Math.random();
+     randomNumber=Math.floor(randomNumber*6);
+ }
+  var playerOneImg=document.getElementById("player-one-img");
+  var playerTwoImg=document.getElementById("player-two-img");
+  var diceImages=["images/dice1.png","images/dice1.png","images/dice1.png","images/dice1.png","images/dice1.png","images/dice1.png"]
+  var R2=["images/dice1.png","images/dice1.png","images/dice1.png","images/dice1.png","images/dice1.png","images/dice1.png"]
+  return generateRandomNumber()
+
+
+
+document.getElementsByClassName("heading").setAttribute("class", "");  
